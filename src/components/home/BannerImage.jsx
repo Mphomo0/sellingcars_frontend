b5aba5a09ef460fc29fdd80a7c870277c291d631
mpho.moipolai <mpho.moipolai@autotrader.co.za>
2024-08-20 10:14:06 +0200

    made css changes

--- src/components/home/BannerImage.jsx
+++ src/components/home/BannerImage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const BannerImage = () => {
   return (
-    <div className='h-52 relative z-0 mb-4'>
+    <div className='h-[3rem] md:h-[5rem] lg:h-52 relative z-0 mb-4'>
       <div className='absolute left-1/2 transform -translate-x-1/2  mt-[-120px] md:mt-[-230px] lg:mt-[-280px] z-50 w-11/12 md:w-10/12 lg:w-9/12 xl:w-8/12'>
         <figure className='flex justify-center'>
           <img
